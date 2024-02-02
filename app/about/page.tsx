a29c9aa729e6bb0a22810e041d6fc90f37eaf9e1
Jacob Templeton <jacob.templeton@gmail.com>
2024-02-01 22:08:13 -0800

    Add responsive design for phones to large desktop on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,14 +7,14 @@ export default function Page() {
         <Image
           src={"/about-img.jpg"}
           fill={true}
-          style={{ objectFit: "cover", objectPosition: "bottom" }}
+          style={{ objectFit: "cover", objectPosition: "40%" }}
           alt="flowers"
         />
       </div>
 
-      <div className="col-start-2 col-span-1 flex flex-col justify-center pt-1 sm:p-4 md:p-10">
+      <div className="col-start-2 col-span-1 flex flex-col justify-center px-3 pt-1 sm:p-4 md:p-10 text-purple-100">
         <h1 className="font-bold sm:text-xl mb-2">Our 1996 beginning</h1>
-        <p className="text-sm sm:text-base mb-2 sm:mb-3">
+        <p className="text-xs sm:text-base mb-2 sm:mb-3">
           Starting in 1996, We developed our Napa property and began
           culitivating Cabernet Sauvignon Wine Grapes, to produce ultra premium
           wines. Many years before our ownership, our property was a plum
@@ -26,7 +26,7 @@ export default function Page() {
           The Wiemer grape vine grown here produces some of the highest quality
           fruit in Napa Valley.
         </p>
-        <p className="text-sm sm:text-base">
+        <p className="text-xs sm:text-base">
           In 2009 we graphed some newly planted rootstock to a special
           Chardonnay bud with lineage dating back to the exceptional and
           historic Wente Chardonnay clone. We released our first Chardonnay wine
