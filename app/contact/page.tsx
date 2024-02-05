fa368f03497beab718e7d075e483e9e06f781642
Jacob Templeton <jacob.templeton@gmail.com>
2024-02-05 14:13:57 -0800

    Add change to size of google maps in desktop and mobile viewport sizes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -57,13 +57,13 @@ export default function Page() {
           <div className="mt-3 flex-1 md:ml-4">
             <textarea
               id="large-input"
-              className="w-full h-full p-4  rounded-lg sm:text-md focus:ring-blue-500 focus:border-blue-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 flex items-center justify-center"
+              className="w-full h-[10rem] md:h-full p-4  rounded-lg sm:text-md focus:ring-blue-500 focus:border-blue-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 flex items-center justify-center"
               placeholder="Message"
             ></textarea>
           </div>
         </form>
       </div>
-      <div className="my-6 min-w-[400px] h-[325px]">
+      <div className="my-6 w-[100%] md:w-[60vw] h-[325px]">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3129.8006929478656!2d-122.34207592486547!3d38.33044597185061!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808500b48515a553%3A0x7994e7355c56ee7e!2sOlney%20Family%20Vineyard%2C%20Cave%20and%20Winery!5e0!3m2!1sen!2sus!4v1707113017835!5m2!1sen!2sus"
           className="border-none"
